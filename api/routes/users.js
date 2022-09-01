30446be84bda0832be422eeb8a4b83e29a2ad77f
gaeun <geun5744@gmail.com>
2022-09-02 00:38:58 +0900

    유저 정보 가져오기 및 삭제

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const Post = require("../models/Post");
 
 // UPDATE
 router.put("/:id", async (req, res) => {
@@ -23,6 +24,37 @@ router.put("/:id", async (req, res) => {
     }
   } else {
     res.status(401).json("You can update only your account!");
+  }
+});
+
+// Delte
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      try {
+        await Post.deleteMany({ username: user.username });
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted...");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } catch (err) {
+      res.status(400).json("User not found!");
+    }
+  } else {
+    res.status(401).json("You can delete only your account!");
+  }
+});
+
+// GET USER
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch {
+    res.status(500).json(err);
   }
 });
 
