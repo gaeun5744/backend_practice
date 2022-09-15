9c309103cb1ca4eb77e67c4357d66f3c6c884eff
gaeun <geun5744@gmail.com>
2022-09-16 01:46:26 +0900

    프론트 데이터 연결

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,13 +3,25 @@ import Posts from "../../components/posts/Posts";
 import Sdiebar from "../../components/sidebar/Sidebar";
 import "./home.css";
 import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts");
+      setPosts(res.data);
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <>
       <Header />
       <div className="home">
-        <Posts />
+        <Posts posts={posts} />
         <Sdiebar />
       </div>
     </>
