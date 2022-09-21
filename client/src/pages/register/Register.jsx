a4f963e55a54195ae853d603a749226f30c60e3c
gaeun <geun5744@gmail.com>
2022-09-21 21:20:15 +0900

    register system

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,31 +1,51 @@
 import "./register.css";
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Register() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await post("/auth/register", {
+      username,
+      email,
+      password,
+    });
+    console.log(res);
+  };
+
   return (
     <div className="register">
       <span className="registerTitle">Register</span>
-      <form className="registerForm">
+      <form className="registerForm" onSubmit={handleSubmit}>
         <label>Username</label>
         <input
           type="text"
           className="registerInput"
           placeholder="Enter your username... "
+          onChange={(e) => setUsername(e.target.value)}
         />
         <label>Email</label>
         <input
           type="text"
           className="registerInput"
           placeholder="Enter your email... "
+          onChange={(e) => setEmail(e.target.value)}
         />
         <label>Password</label>
         <input
           type="password"
           className="registerInput"
           placeholder="Enter your password..."
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <button className="registerButton">register</button>
+        <button className="registerButton" type="submit">
+          register
+        </button>
       </form>
       <button className="registerLoginButton">
         <Link to="/login" className="link">
