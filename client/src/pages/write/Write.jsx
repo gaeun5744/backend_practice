978c8a8163724c6ee43a79d378b7ca01c5a0b283
gaeun <geun5744@gmail.com>
2022-11-04 01:47:30 +0900

    wrtie.jsx 이미지 넣기 기능 추가

--- client/src/pages/write/Write.jsx
+++ client/src/pages/write/Write.jsx
@@ -1,20 +1,55 @@
+import axios from "axios";
 import React from "react";
+import { useContext } from "react";
+import { useState } from "react";
+import { Context } from "../../context/Context";
 import "./write.css";
 
 export default function Write() {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [file, setFile] = useState(null);
+  const { user } = useContext(Context);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPost = {
+      username: user.username,
+      title,
+      desc,
+    };
+    if (file) {
+      const data = FormData();
+      const filename = Date.now() + file.name;
+      data.append("name", filename);
+      data.append("file", file);
+      newPost.photo = filename;
+      try {
+        await axios.post("/upload", data);
+      } catch (err) {}
+    }
+    try {
+      const res = await axios.post("/posts", newPost);
+      window.location.replace("/post/" + res.data._id);
+    } catch (err) {}
+    axios.post("/posts", newPost);
+  };
   return (
     <div className="write">
-      <img
-        className="writeImg"
-        src="https://images.pexels.com/photos/7469433/pexels-photo-7469433.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        alt=""
-      />
-      <form className="writeForm">
+      {file && (
+        <img className="writeImg" src={URL.createObjectURL(file)} alt="" />
+      )}
+      <form className="writeForm" onSubmit={handleSubmit}>
         <div className="writeFormGroup">
           <label htmlFor="fileInput">
             <i className="writeIcon fa-solid fa-plus"></i>
           </label>
-          <input type="file" id="fileInput" style={{ display: "none" }} />
+          <input
+            type="file"
+            id="fileInput"
+            style={{ display: "none" }}
+            onChange={(e) => setFile(e.target.files[0])}
+          />
           <input
             type="text"
             placeholder="Title"
@@ -29,7 +64,9 @@ export default function Write() {
             className="writeInput writeText"
           ></textarea>
         </div>
-        <button className="writeSubmit">Publish</button>
+        <button className="writeSubmit" type="submit">
+          Publish
+        </button>
       </form>
     </div>
   );
